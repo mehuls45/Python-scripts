1bfaaac7aea0aee464f558af615963989b92b135
Mehul Solanki <mehulsolanki45@gmail.com>
2017-04-25 15:31:47 +0530

    Added CussWordScanner.py

---
+++ CussWordScanner.py
@@ -0,0 +1,26 @@
+#! python3
+# CussWordScanner
+
+cuss = {'fuck':'****','cunt':'****','asshole':'******','bastard':'*******',
+        'fucker':'******','fucking':'*******','bastards':'********',
+        'cunts':'*****','assholes':'********'
+        }
+
+import pyperclip
+
+text = pyperclip.paste()
+
+words = text.split(' ')
+
+print(words)
+
+for i in range (len(words)):
+    
+    word = words[i]
+    word = word.lower()
+
+    if word in cuss.keys():
+        words[i] = cuss[word]
+
+text = ' '.join(words)
+pyperclip.copy(text)
